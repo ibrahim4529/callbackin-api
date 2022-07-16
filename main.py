cbd065c24a0e12a8c348ed41ebe8660f8d39c1be
ibrahim <ibrahim.hanif4529@gmail.com>
2022-07-16 20:59:20 +0700

    init

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+from pydoc import cli
+from flask import Flask, jsonify, request
+from paho.mqtt import client as mqtt_client
+
+app = Flask(__name__)
+
+@app.route("/<path>", methods=["GET", "POST"])
+def callback(path):
+    if request.is_json:
+        body = request.get_json()
+    else:
+        body = "none data json"
+    headers = dict(request.headers)
+    client = mqtt_client.Client()
+    client.connect("mqtt.eclipseprojects.io")
+    payload = json.dumps({
+        "body": body,
+        "headers": headers,
+    })
+    client.publish(topic=path, payload=payload)
+    client.disconnect()
+    return "success"
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000, debug=True)
