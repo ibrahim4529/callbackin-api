2455543b615ef426f046a56a747c143ae6c46885
ibrahim <ibrahim.hanif4529@gmail.com>
2022-07-16 21:22:19 +0700

    fix

--- main.py
+++ main.py
@@ -23,5 +23,3 @@ def callback(path):
     return "success"
 
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from app.main import app
+
+if __name__ == "__main__":
+    app.run()
